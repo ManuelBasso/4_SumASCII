4c031eb8f5e0e4cfe029f885ea9c58af82678a18
ManuelBasso <146716888+ManuelBasso@users.noreply.github.com>
2023-10-10 13:54:24 +0200

    Add files via upload

---
+++ 4_SumASCII.java
@@ -0,0 +1,12 @@
+//Dati due caratteri, restituire la somma dei loro valori ASCII
+
+public class SumASCII {
+    public static void main(String args[]) {
+      char first = 'P';
+      char second = 'A';
+      
+      int somma = (int)first + (int)second;
+      
+      System.out.println("Sum ASCII 1st and 2nd char  = " + somma);
+    }
+}
